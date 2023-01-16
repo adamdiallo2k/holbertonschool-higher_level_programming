b77121efe3d749e848dc51991d6e9bbfa40c84b2
adamdiallo2k <adamdiallo2k@gmail.com>
2023-01-16 12:25:20 -0800

    1-last_digit.py

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,10 +7,11 @@ if (number < 0):
     absolute = number * -1 % 10
     absoluteString = '-'
 
-
-if (absolute % 10 > 5):
+if (number % 10 > 5):
     print('Last digit of', number,'is', absoluteString + str(absolute), 'and is greater than 5')
-if (absolute % 10 < 5 and absolute % 10 > 0):
-    print('Last digit of', number,'is',absoluteString + str(absolute), 'and is less than 5 and not 0')
-if (absolute % 10 == 0) :
-    print('Last digit of', number,'is',absolute, 'and is 0')
+
+if (number % 10 < 5 and absolute % 10 > 0):
+    print('Last digit of', number,'is', absoluteString + str(absolute), 'and is less than 5 and not 0')
+
+if (number % 10 == 0):
+    print('Last digit of', number,'is', absolute, 'and is 0')
