7b1fdfb539901eabb8d7912967e8d873f192c8d8
adamdiallo2k <adamdiallo2k@gmail.com>
2023-01-16 12:16:14 -0800

    task 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-absolute = number
+absolute = number % 10
+absoluteString = ""
 if (number < 0):
-    absolute = number * -1
+    absolute = number * -1 % 10
+    absoluteString = "-"
+
+
 if (absolute % 10 > 5):
-    print('Last digit of', number,'is',absolute % 10,'and is greater than 5')
+    print('Last digit of', number,'is', absoluteString, absolute, 'and is greater than 5')
 if (absolute % 10 < 5 and absolute % 10 > 0):
-    print('Last digit of', number,'is',absolute % 10,'and is less than 5')
+    print('Last digit of', number,'is',absoluteString, absolute, 'and is less than 5')
 if (absolute % 10 == 0) :
-    print('Last digit of', number,'is',absolute % 10,'and is 0')
+    print('Last digit of', number,'is',absolute, 'and is 0')
