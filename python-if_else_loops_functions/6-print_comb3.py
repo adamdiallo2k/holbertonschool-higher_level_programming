a5130c8f04c42e9c3fbf786f7ff0e42689798561
adamdiallo2k <adamdiallo2k@gmail.com>
2023-01-17 12:22:42 -0800

    task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,5 +3,5 @@ numbers = [i for i in range(10)]
 output = []
 for i in range(len(numbers)):
     for j in range(i+1, len(numbers)):
-        output.append(f"{numbers[i]:d}{numbers[j]:d}")
+        output.append("{}{}".format(numbers[i], numbers[j]))
 print(", ".join(output))
