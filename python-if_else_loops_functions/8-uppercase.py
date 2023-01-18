3ccebfece2a0538f79db5c6b00cadbccb3fd5e3e
adamdiallo2k <adamdiallo2k@gmail.com>
2023-01-18 01:50:28 -0800

    task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+def uppercase(str):
+    result = ""
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+            result += chr(ord(str[i]) - 32)
+        else:
+            result += str[i]
+    print(result)
