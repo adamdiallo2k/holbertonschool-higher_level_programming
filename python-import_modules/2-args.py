45b58d69aa518d676788a3c3597a916110084f48
adamdiallo2k <adamdiallo2k@gmail.com>
2023-01-19 07:29:57 -0800

    fix the end error

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     argv = sys.argv
     if (argc - 1 == 0):
         print("{} arguments.".format(argc - 1))
+    elif (argc - 1 == 1):
+        print("{}".format("1 argument:"))
+        print("1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(argc - 1))
         for i in range(1, argc):
