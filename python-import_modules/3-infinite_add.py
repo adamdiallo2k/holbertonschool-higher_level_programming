47f425bf703778c849550bf5f348e8242d2cc141
adamdiallo2k <adamdiallo2k@gmail.com>
2023-01-20 04:28:53 -0800

    fix the indentation error

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-        import sys
-        argv = sys.argv
-        sumnumber = 0
-        for i in range(1, len(argv)):
-            sumnumber += int(argv[i])
-        print("{}".format(sumnumber))
+    import sys
+    argv = sys.argv
+    sumnumber = 0
+    for i in range(1, len(argv)):
+        sumnumber += int(argv[i])
+    print("{}".format(sumnumber))
