d7c48479df4a5cc7660e554e37e599244c74b347
adamdiallo2k <adamdiallo2k@gmail.com>
2023-01-23 01:38:31 -0800

    task 3

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    y = len(my_list) - 1
+    for i in range(len(my_list)):
+        print("{}".format(my_list[y]))
+        y = y - 1
