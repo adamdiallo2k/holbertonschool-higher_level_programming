023ff02c36617084d851782d4b550cf24fa2d21a
adamdiallo2k <adamdiallo2k@gmail.com>
2023-01-24 03:42:46 -0800

    fix the spaces

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -11,6 +11,6 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{:d}".format(my_list[i]), end='')
         print("{}".format(''))
-        return  x
+        return x
     except IndexError as e:
         print(f"{e}")
