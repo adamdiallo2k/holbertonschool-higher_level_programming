01dec9614359a4c8af42fe6546baf908b7e3c6e9
adamdiallo2k <adamdiallo2k@gmail.com>
2023-01-30 01:36:23 -0800

    squre with private attribute size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,14 +6,15 @@ The Square class represents a square and has the following attributes:
 - size
 """
 
+
 class Square:
     """This class represents a square.
-    
+
     It has the following attributes:
     - size
     """
     __size = 0
 
-    def __init__(self,new_size):
+    def __init__(self, new_size):
         """Initialize a new Square with a given size."""
         self.__size = new_size
