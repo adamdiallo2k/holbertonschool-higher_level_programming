aa97c1b3b14881e5607c614a528275095a4b0154
adamdiallo2k <adamdiallo2k@gmail.com>
2023-02-03 10:07:03 -0800

    task 3

---
+++ 3-say_my_name.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" module """
+def say_my_name(first_name, last_name=""):
+    """
+    This function takes first name and last name as input and prints "My name is <first name> <last name>"
+    
+    Args:
+    first_name (str): The first name of the person.
+    last_name (str, optional): The last name of the person. Default is an empty string.
+    
+    Returns:
+    None
+    
+    Raises:
+    TypeError: If first_name or last_name is not a string.
+    
+    Example:
+    >>> say_my_name("John", "Doe")
+    My name is John Doe
+    >>> say_my_name("Jane")
+    My name is Jane
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is", first_name, last_name)
+
