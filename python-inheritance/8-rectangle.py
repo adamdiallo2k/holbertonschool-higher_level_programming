77c625b0fe7d2b42e8a102785b3fe65c343958f8
adamdiallo2k <adamdiallo2k@gmail.com>
2023-02-08 14:25:40 -0800

    task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """ Module """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """ class doc """
     def __init__(self, width, height):
