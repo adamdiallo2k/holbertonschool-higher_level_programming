d9128cfb322cd0082d2ac7296e51641c9a3af33c
adamdiallo2k <adamdiallo2k@gmail.com>
2023-02-08 02:24:54 -0800

    task 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,20 +1,24 @@
-#!/usr/bin/python3
-""" module """
 class MyList(list):
+    """ Module """
     def print_sorted(self):
         """
-        This function takes a list as an argument, sorts a copy of the list in ascending order using a for loop, and then prints the sorted list. The original list is not modified.
-
-        Args:
-        lst (list): The list to be sorted and printed.
+        Prints a sorted copy of the list in ascending order. The original list remains unchanged.
 
         Returns:
         None
         """
         sorted_list = self[:]
+        """
+        Create a copy of the original list to sort, so the original list remains unchanged.
+        """
         for i in range(len(sorted_list)):
             for j in range(i + 1, len(sorted_list)):
                 if sorted_list[i] > sorted_list[j]:
                     sorted_list[i], sorted_list[j] = sorted_list[j], sorted_list[i]
+        """
+        Sort the copied list using a nested for loop and swap elements if the current element is greater than the next element.
+        """
         print(sorted_list)
-
+        """
+        Print the sorted list.
+        """
