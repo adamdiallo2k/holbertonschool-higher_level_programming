16d67df27a4859dfdfdc2e4f92fef097d2a0af5b
adamdiallo2k <adamdiallo2k@gmail.com>
2023-02-08 13:44:39 -0800

    task 8

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Module """
+
+class BaseGeometry:
+    """ class doc """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ doc """
+        if isinstance(value, bool):
+            raise TypeError(f"{name} must be an integer")
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """ class doc """
+
+    def __init__(self, width, height):
+        super().integer_validator('width', width)
+        super().integer_validator('height', height)
+        self.width = width
+        self.height = height
