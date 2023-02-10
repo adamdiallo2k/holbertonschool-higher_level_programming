6ee40b544ef02a494776a37da54b06a36f053b26
adamdiallo2k <adamdiallo2k@gmail.com>
2023-02-10 07:59:43 -0800

    task 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,18 +4,21 @@ Module for writing a string to a text file.
 """
 
 def append_write(filename="", text=""):
-    
+    """
+    Append a string to a text file.
+
+    Arguments:
+    filename (str, optional): The name of the file to be written. Defaults to an empty string.
+    text (str, optional): The string to be written to the file. Defaults to an empty string.
+
+    Returns:
+    int: The number of characters written to the file. Returns 0 if no `filename` is provided.
+    """
     if (filename):
-        """
-        Write a string to a text file.
-
-        Arguments:
-        filename (str, optional): The name of the file to be written. Defaults to an empty string.
-        text (str, optional): The string to be written to the file. Defaults to an empty string.
-
-        Returns:
-        int: The number of characters written to the file.
-        """
         with open(filename, "a", encoding="utf-8") as file:
+            # Write the text to the file
             content = file.write(text)
+        # Return the number of characters written to the file
         return len(text)
+    else:
+        return 0
