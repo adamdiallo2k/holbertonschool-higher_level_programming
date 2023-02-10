5d220576422857873dc67b4986728b4b00c2c8b1
adamdiallo2k <adamdiallo2k@gmail.com>
2023-02-10 09:47:45 -0800

    task 4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,3 +1,22 @@
+#!/usr/bin/python3
+"""
+Module for converting JSON strings into Python objects.
+
+This module provides a single function, `from_json_string`, which takes a JSON string as input and returns a
+Python object representation of that string. The conversion is performed using the `json.loads` function
+from the `json` module, which is a built-in module in Python.
+
+Functions:
+    from_json_string(my_str)
+        Converts a JSON string into a Python object.
+        
+        Arguments:
+            my_str (str) -- the JSON string to be converted
+        
+        Returns:
+            A Python object representation of the JSON string.
+"""
+
 import json
 
 def from_json_string(my_str):
