caa2774e049d2bc7f9fb42e6c00953060dd55155
adamdiallo2k <adamdiallo2k@gmail.com>
2023-02-09 22:45:57 -0800

    task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,25 @@
 #!/usr/bin/python3
+""" Module """
 def read_file(filename=""):
+    """
+    This function takes a filename as an argument and prints the content of the file.
+    If no filename is provided, it will print an empty string.
+
+    Arguments:
+    filename (str): The name of the file to be read.
+
+    Returns:
+    None
+    """
     with open(filename, "r") as file:
+        """
+        Open the file using a with statement to ensure that the file is properly closed after reading.
+        """
         content = file.read()
+        """
+        Read the entire content of the file into a variable called content.
+        """
     print(content)
+    """
+    Print the content of the file.
+    """
