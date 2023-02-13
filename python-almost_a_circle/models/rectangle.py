5c327fb7e58088e00f5f4b04e32c9f2532f1b098
adamdiallo2k <adamdiallo2k@gmail.com>
2023-02-13 08:25:22 -0800

    Write the class Rectangle that inherits from Base

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 
 """
-This module defines a Rectangle 
+This module defines a Rectangle
 class, which inherits from the Base class,
 for creating rectangle objects with unique integer IDs.
 """
 
 from models.base import Base
 
+
 class Rectangle(Base):
+
+
     """
     A class for creating rectangle 
     objects with unique integer IDs.
