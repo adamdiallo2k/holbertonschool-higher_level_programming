b048751a0b81825a1037d644098bb6628f498a9d
adamdiallo2k <adamdiallo2k@gmail.com>
2023-02-13 07:35:59 -0800

    task 1

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module defines a Base class for creating objects with unique integer IDs.
+"""
+
+class Base:
+    """
+    A class for creating objects with unique integer IDs.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initialize a Base object with a unique ID.
+
+        Args:
+            id (int, optional): The ID to assign to the object. If not provided, a new unique ID will be generated.
+
+        Attributes:
+            id (int): The ID assigned to the object.
+
+        Returns:
+            None.
+        """
+        if (id is not None):
+            self.id = id
+        else:
+            self.__class__.__nb_objects += 1
+            self.id = self.__class__.__nb_objects
