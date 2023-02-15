3ac8d73e105ecd2765730a2caf12ac9dce6067ec
adamdiallo2k <adamdiallo2k@gmail.com>
2023-02-14 16:24:23 -0800

    test

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,7 +8,6 @@ for creating rectangle objects with unique integer IDs.
 
 from models.base import Base
 
-
 class Rectangle(Base):
 
     """
@@ -18,6 +17,7 @@ class Rectangle(Base):
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+    
         """
         Initialize a Rectangle object with a unique
         ID and the specified width, height,
@@ -45,25 +45,25 @@ class Rectangle(Base):
         Returns:
             None.
         """
-        if not isinstance(height, int):
-            raise TypeError("height must be an integer")
-        if height <= 0:
-            raise ValueError("height must be > 0")
 
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
-        if width <= 0:
+        elif width <= 0:
             raise ValueError("width must be > 0")
 
-        if not isinstance(x, int):
-            raise TypeError("x must be integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        elif height <= 0:
+            raise ValueError("height must be > 0")
 
-        if x <= 0:
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        elif x < 0:
             raise ValueError("x must be >= 0")
 
         if not isinstance(y, int):
             raise TypeError("y must be an integer")
-        if y <= 0:
+        elif y < 0:
             raise ValueError("y must be >= 0")
 
         super().__init__(id)
@@ -74,99 +74,49 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """
-        Get the width of the rectangle.
-
-        Returns:
-            The width of the rectangle.
-        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        Set the width of the rectangle.
-
-        Args:
-            value (int): The new width of the rectangle.
-
-        Returns:
-            None.
-        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         self.__width = value
+
     @property
     def height(self):
-        """
-        Get the height of the rectangle.
-
-        Returns:
-            The height of the rectangle.
-        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        Set the height of the rectangle.
-
-        Args:
-            value (int): The new height of the rectangle.
-
-        Returns:
-            None.
-        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
     def x(self):
-        """
-        Get the x-coordinate of the upper-left
-        corner of the rectangle.
-
-        Returns:
-            The x-coordinate of the upper-left
-            corner of the rectangle.
-        """
         return self.__x
 
     @x.setter
     def x(self, value):
-        """
-        Set the x-coordinate of the upper-left
-        corner of the rectangle.
-
-        Args:
-            value (int): The new x-coordinate of the
-            upper-left corner of the rectangle.
-
-        Returns:
-            None.
-        """
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
     def y(self):
-        """
-        Get the y-coordinate of the
-        upper-left corner of the rectangle.
-
-        Returns:
-            The y-coordinate of the upper-left
-            corner of the rectangle.
-        """
         return self.__y
 
     @y.setter
     def y(self, value):
-        """
-        Set the y-coordinate of the upper-left
-        corner of the rectangle.
-
-        Args:
-            value (int): The new y-coordinate of
-            the upper-left corner of the rectangle.
-
-        Returns:
-            None.
-        """
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
+
