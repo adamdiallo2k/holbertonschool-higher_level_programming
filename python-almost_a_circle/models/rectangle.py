061e2fa49e6bbc6ddf9090954e213d31fbf66d4b
adamdiallo2k <adamdiallo2k@gmail.com>
2023-02-15 10:16:54 -0800

    task 7

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -155,4 +155,28 @@ class Rectangle(Base):
         object in the format: [Rectangle] (<id>) <x>/<y> <width>/<height>
         """
         return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
+    def update(self, *args):
+        """
+    Update the Rectangle attributes using *args argument.
 
+    Args:
+        *args: Variable length argument list.
+            args[0] (int, optional): The ID of the rectangle.
+            args[1] (int, optional): The width of the rectangle.
+            args[2] (int, optional): The height of the rectangle.
+            args[3] (int, optional): The x-coordinate of the rectangle.
+            args[4] (int, optional): The y-coordinate of the rectangle.
+
+    Returns:
+        None.
+    """
+        if len(args) > 0:
+            self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
