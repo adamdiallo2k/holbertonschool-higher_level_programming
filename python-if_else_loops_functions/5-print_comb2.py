2d66239209c1280abdae65e045a10258e74bd6f2
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-01 14:57:47 +0200

    task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-for num in range(99):
-    if num != 98:
-        print("{:02d}".format(num), end=', ')
-    else:
-        print("{:02d}".format(num))
+for i in range(100):
+    print("{:02d}".format(i), end=", " if i != 99 else "\n")
 
