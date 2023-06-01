ae186bc0ebf8392341639ad7658a3df2b4790d8e
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-01 22:49:41 +0200

    task 6

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 
-for ch in range(97, 123):
-    print("{:c}".format(ch), end='')
+for i in range(10):
+    for j in range(i+1, 10):
+        if i == 8 and j == 9:
+            print(f'{i}{j}')
+        else:
+            print(f'{i}{j}', end=', ')
