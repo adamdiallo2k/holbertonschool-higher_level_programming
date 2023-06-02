c759e17125bd1216a73f16bade59244269670ac3
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-02 23:13:50 +0200

    test

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 num_args = len(sys.argv) - 1
 
 if (num_args == 1):
-    print("1 argument:")
+    print("{} argument:".format(num_args))
     print("1: {}".format(sys.argv[1]))
 
 else:
