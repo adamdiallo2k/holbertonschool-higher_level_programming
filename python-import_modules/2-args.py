edd2250571c005386ef0f8410d03af988ce642b9
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-02 23:08:48 +0200

    test

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+num_args = len(sys.argv) - 1
+
+if (num_args == 1):
+    print("1 argument:")
+    print("1: {}".format(sys.argv[1]))
+
+else:
+    print("{} arguments".format(num_args))
+    if (num_args > 0):
+        i = 1
+        for i in range(num_args):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
