07231421050e0bb81cc892c56db9436b3e90541f
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-02 22:24:46 +0200

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""
+This script adds two numbers using a function imported from another module.
+"""
+
 if __name__ == "__main__":
+    """
+    This block is executed if the script is run directly, not imported as a module.
+    """
     from add_0 import add
 
 a = 1
 b = 2
+
 print("{} + {} = {}".format(a, b, add(a, b)))
