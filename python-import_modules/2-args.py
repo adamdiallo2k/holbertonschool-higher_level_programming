22fa3b98c5c11c414fb76fde4c9c52a8925dfe26
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-02 23:17:08 +0200

    test

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
+import sys
+
+def print_args():
+    num_args = len(sys.argv) - 1  # Subtract 1 to exclude the script name
+    if num_args == 0:
+        print("Number of arguments: 0.")
+    elif num_args == 1:
+        print("Number of argument: 1:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("Number of arguments: {}:".format(num_args))
+        for i in range(1, num_args + 1):
+            print("{}: {}".format(i, sys.argv[i]))
+
 if __name__ == "__main__":
-    import sys
-
-num_args = len(sys.argv) - 1
-
-if (num_args == 1):
-    print("{} argument:".format(num_args))
-    print("1: {}".format(sys.argv[1]))
-
-else:
-    print("{} arguments".format(num_args))
-    if (num_args > 0):
-        i = 1
-        for i in range(num_args):
-            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    print_args()
