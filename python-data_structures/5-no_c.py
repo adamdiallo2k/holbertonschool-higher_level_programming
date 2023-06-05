1a444902a06a9c5fd90c09072fb50de087306ddb
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-05 16:52:09 +0200

    task 5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    my_string2 = ""
-    for i in range(len(my_string)):
-       if not (my_string[i] == 'c' or my_string[i] == 'C'):
-            my_string2 += my_string[i]
-    return my_string2
+    return ''.join(c for c in my_string if c != 'c' and c != 'C')
+
