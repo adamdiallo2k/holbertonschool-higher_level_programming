10cac8b3e810acdd5168585dacc860f33794c724
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-05 17:14:58 +0200

    test

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    if matrix == None:
+    if matrix == [[]]:
         print()
-    for i in range(len(matrix)):
-        for y in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][y]), end=' ')
-        print("{}".format(""))
+    else:
+        for i in range(len(matrix)):
+            for y in range(len(matrix[i])):
+                print("{:d}".format(matrix[i][y]), end=' ')
+            print("{}".format(""))
