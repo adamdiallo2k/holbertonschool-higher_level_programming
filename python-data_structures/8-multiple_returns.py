50df86721c4dff2b41e08812d8d54288d703e333
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-05 21:09:18 +0200

    task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 
 def multiple_returns(sentence):
     if sentence == "":
-        return None
+        return 0, None
     tuple_c = tuple( word for word in sentence)
     return len(tuple_c), tuple_c[0]
 
