10ba4795c4e846c8029afced093268dac7573371
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-06 23:12:13 +0200

    task 2

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    if my_list is None:
+        return None
+    
+    new_list = [89 if x == 2 else x for x in my_list]
+    return new_list
