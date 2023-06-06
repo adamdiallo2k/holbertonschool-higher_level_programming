fd3a721fc2b04b4f0c4c68b6abdbf82744a393d2
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-06 23:15:48 +0200

    forgot to put the variable search

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -5,5 +5,5 @@ def search_replace(my_list, search, replace):
     if my_list is None:
         return None
     
-    new_list = [replace if x == 2 else x for x in my_list]
+    new_list = [replace if x == search else x for x in my_list]
     return new_list
