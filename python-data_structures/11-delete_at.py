4f2881a800149a914ed30a2e5a663ca9d38a2500
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-06 22:06:32 +0200

    task 11

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=[], idx=0):
+    if my_list == None:
+        return None
+    
+    if (len(my_list) < idx or idx < 0):
+        return my_list
+    
+    del my_list[idx]
+    return my_list
