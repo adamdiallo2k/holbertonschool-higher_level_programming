cf9615612d72ddb69292d162d90d8d50e82852b6
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-06 14:40:22 +0200

    edit task

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@
 def element_at(my_list, idx):
    if my_list == None:
       return None
-   if not (0 <= idx <= len(my_list)):
+   if not (0 <= idx < len(my_list)):
      return None
    return my_list[idx]
