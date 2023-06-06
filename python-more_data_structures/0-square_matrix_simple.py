2c83137ddf81637fdafbcdd9f5f82a39f86bf297
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-06 22:58:24 +0200

    test

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
+
 def square_matrix_simple(matrix=[]):
-    if matrix is None:
-        return None
     
     new_matrix = [[element ** 2 for element in row] for row in matrix]
 
