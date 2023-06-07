454a06f31b6f640461debb9c717353469fa0a152
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-07 22:22:05 +0200

    test

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    return  {key: Value * 2 for key, Value in a_dictionary.items() }
+    return  {key: Value * 2 for key, Value in a_dictionary.items()}
