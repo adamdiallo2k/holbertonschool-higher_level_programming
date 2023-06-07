f7aac83b8b95861274c6b4a80ea590e3f3104eba
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-07 12:12:34 +0200

    test

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
-    set_3 = []
+    set_3 = set()
     for x in set_1:
         if x not in set_2 and x not in set_3:
-            set_3.append(x)
+            set_3.add(x)
     for y in set_2:
-        if y not in set_1 and x not in set_3:
-            set_3.append(y)
+        if y not in set_1 and y not in set_3:
+            set_3.add(y)
     return set_3
