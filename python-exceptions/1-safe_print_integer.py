4d0cb66ca749dc3deb009c0bd88740d4937a33e0
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-08 21:35:20 +0200

    test

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,6 +4,6 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
     except:
-        if (isinstance(value, int) == False):
+        if not isinstance(value, int):
             return False
     return True
