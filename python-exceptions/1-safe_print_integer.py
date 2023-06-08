529d45f3f7582d44e35a40c2a26207b1e2e84143
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-08 21:32:28 +0200

    task 1

---
+++ python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except:
+        if isinstance(value,int) == False:
+            return False
+    return True
