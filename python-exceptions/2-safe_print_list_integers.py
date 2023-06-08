f471636dab6e4865ab2d88f06496780f9e490e00
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-08 21:57:31 +0200

    task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0):
+def safe_print_list_integers(my_list=[], x=0):
     num_printed = 0
     for i in range(x):
         try:
-            print(my_list[i], end="")
+            print("{:d}".format(my_list[i]), end="")
             num_printed += 1
         except IndexError:
             break
+        except (ValueError, TypeError):
+            continue
     print()
     return num_printed
