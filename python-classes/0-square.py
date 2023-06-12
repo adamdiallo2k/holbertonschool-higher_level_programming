edebe63e38bd3dbe409961e358616188490cf19f
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-12 21:35:05 +0200

    test

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -9,6 +9,3 @@ class Square:
     This class represents a square. 
     More detailed information about the class can be provided here.
     """
-    
-if __name__ == "__main__":
-    # Test code can be placed here. It will only be run when this file is executed directly, not when it is imported.
