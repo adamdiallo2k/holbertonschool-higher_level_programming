3877e96e0bcadc74ea837544ac9e90d3863ed032
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-12 21:34:27 +0200

    test

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-"""Module for Square"""
+
+"""
+This is a module for Square.
+"""
+
 class Square:
-    """ Content of square"""
+    """ 
+    This class represents a square. 
+    More detailed information about the class can be provided here.
+    """
+    
+if __name__ == "__main__":
+    # Test code can be placed here. It will only be run when this file is executed directly, not when it is imported.
