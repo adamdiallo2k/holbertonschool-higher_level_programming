543c58e9b06da1d30a5bff767eb2d944bca359ab
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-12 21:38:33 +0200

    test

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 This is a module for Square.
 """
@@ -9,3 +8,4 @@ class Square:
     This class represents a square. 
     More detailed information about the class can be provided here.
     """
+    pass
