e1be268269a5ed5d48ca6e085aa6cdf8657ba792
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-13 13:35:20 +0200

    comment setter and getter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,16 +3,17 @@
 
 
 class Square:
-
     """A class to represent a square with methods to calculate its area."""
     def __init__(self, size=0):
         self.__size = size
     @property
     def size(self):
+        """Return the size of the square"""
         return self.__size
     
     @size.setter
     def size(self, value):
+        """set the size by verifing if its an int and superior than 0"""
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if value < 0:
