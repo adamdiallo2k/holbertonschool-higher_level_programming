15e5aa1e3226e433d89585a98001fba44abd1e33
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-13 13:33:16 +0200

    task 4

---
+++ python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""A module for representing a square."""
+
+
+class Square:
+
+    """A class to represent a square with methods to calculate its area."""
+    def __init__(self, size=0):
+        self.__size = size
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """Calculate and return the area of the square."""
+        return self.__size ** 2
