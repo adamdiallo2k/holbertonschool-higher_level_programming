03fcf6bb8ae36aa4ee37df95a0cf910d392479dc
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-13 14:05:04 +0200

    task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""A module for representing a square."""
+
+
+class Square:
+    """A class to represent a square with methods to calculate its area."""
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size is 0:
+            print("")
+
+        for i in range(self.__size):
+            for y in range(self.__size):
+                print("{}".format('#'),end="")
+            print("")
