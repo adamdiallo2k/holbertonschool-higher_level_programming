a68382ab0e19e018d51a02f9e319ce7b512c10fa
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-14 17:32:21 +0200

    tesr

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""module commented"""
+
 
 def add_integer(a, b=98):
+    """commented function"""
     if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer or a float')
     if not isinstance(b, (int, float)):
