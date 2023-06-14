ad4ed149f9076e3f5fcfe0df1dce657b0705ec52
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-14 13:26:24 +0200

    test

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -4,18 +4,20 @@
 
 class Square:
     """A class to represent a square with methods to calculate its area."""
+
     def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new Square."""
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """gets the size of the square"""
+        """Get or set the size of the Square."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """set the size"""
+        """Set the size of the Square."""
         if not isinstance(value, int):
             raise TypeError('size must be an integer')
         if value < 0:
@@ -24,12 +26,12 @@ class Square:
 
     @property
     def position(self):
-        """gets the position of the square"""
+        """Get or set the position of the Square."""
         return self.__position
 
     @position.setter
     def position(self, value):
-        """set the position"""
+        """Set the position of the Square."""
         valid = 0
         if not isinstance(value, tuple) or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -41,9 +43,9 @@ class Square:
                     valid += 1
             if valid == 2:
                 self.__position = value
-                
+
     def area(self):
-        """Return the area of the square"""
+        """Return the current area of the square."""
         return self.__size ** 2
 
     def my_print(self):
@@ -51,5 +53,5 @@ class Square:
         if self.__size == 0:
             print("")
         else:
-             print("\n" * self.position[1], end="")
-             print("\n".join(" " * self.position[0] + "#" * self.__size for _ in range(self.__size)))
+            print("\n" * self.position[1], end="")
+            print("\n".join(" " * self.position[0] + "#" * self.__size for _ in range(self.__size)))
