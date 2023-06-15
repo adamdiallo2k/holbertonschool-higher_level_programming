24b2734a3fb0a73843a9dc6a88be10e4ef80c88d
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-16 00:25:46 +0200

    test

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""module commented"""
+"""module that contains the function text_indentation(text)"""
+
 
 def text_indentation(text):
-    """function commented"""
-    if not isinstance(text, str):
-        raise TypeError('text must be a string')
+    """prints a text with 2 new lines after
+    each of these characters: ., ? and :"""
+    if isinstance(text, str) is False:
+        raise TypeError("text must be a string")
 
-    symbols = ['.', '?', ':']
-    for symbol in symbols:
-        text = text.replace(symbol, symbol + '\n\n')
-
-    lines = text.split('\n')
-    for i in range(len(lines)):
-        lines[i] = lines[i].strip()
-
-    print('\n'.join(lines))
+    after_new_line = False
+    for c in text:
+        if after_new_line:
+            if c == " ":
+                continue
+            after_new_line = False
+        if c in ['.', '?', ':']:
+            print(c)
+            print("")
+            after_new_line = True
+        else:
+            print(c, end="")
