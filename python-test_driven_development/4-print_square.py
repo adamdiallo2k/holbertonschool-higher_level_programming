efb7e273f7fec8dd38afc74a0d7ef8bc331b9be4
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-16 00:35:37 +0200

    test

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""Module """
+""" module with function print_square(size) """
 
 
 def print_square(size):
-    """function"""
-    if not isinstance(size, int):
-        raise TypeError('size must be an integer')
+    """prints a square with the character #"""
+    if isinstance(size, int) is False:
+        raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError('size must be >= 0')
+        raise TypeError("size must be >= 0")
+    if isinstance(size, float) is True and size < 0:
+        raise TypeError("size must be an integer")
 
-    for _ in range(size):
-        print('#' * size)
+    for i in range(size):
+        print("#" * size)
