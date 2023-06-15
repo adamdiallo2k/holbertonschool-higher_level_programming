c2cfd9cb0f3e65e3528ca898fcfe65682529fcb8
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-16 00:32:33 +0200

    test

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""module commented"""
 
 
 def matrix_divided(matrix, div):
+    """function commented"""
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
 
