71a7d167c16d5a63e3f2e44b7dbdd39489d2a015
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-16 00:12:52 +0200

    test

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """Module """
+
+
 def print_square(size):
     """function"""
     if not isinstance(size, int):
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
-    
+
     for _ in range(size):
         print('#' * size)
