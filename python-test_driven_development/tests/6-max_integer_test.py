244aa1e4346559dae19c60b18907bf9db314d904
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-16 21:07:00 +0200

    test

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,20 @@
+import unittest
+from max_integer import max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    def test_max_integer(self):
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+        self.assertEqual(max_integer([1]), 1)
+
+    def test_max_integer_errors(self):
+        with self.assertRaises(TypeError):
+            max_integer([1, 2, '3', 4])
+        with self.assertRaises(TypeError):
+            max_integer(1234)
+
+if __name__ == '__main__':
+    unittest.main()
+
