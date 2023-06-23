39c6f9f19737a1e5e65bd16adaf5bef5652f59f3
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-23 23:02:47 +0200

    teest

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""module that adds all arguments to a Python list, and then saved to a file"""
+
+
+def class_to_json(obj):
+    """commented function"""
+    return obj.__dict__
