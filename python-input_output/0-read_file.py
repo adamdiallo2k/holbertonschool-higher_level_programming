6e95906fff4e63337045f19296dea79fcd8ffb6a
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-23 21:01:39 +0200

    task 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" commented module"""
+
+
+def read_file(filename=""):
+    """commented function"""
+    with open(filename, "r", encoding='utf8') as f:
+        print(f.read())
