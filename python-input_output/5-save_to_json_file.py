1179f73949d201c4a0b2da531e1ae1cb6bf3746c
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-23 22:26:09 +0200

    task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,7 +5,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """commented function"""
-    my_obj = list(my_obj)
     with open(filename, "w", encoding="utf-8") as f:
        f.write(json.dumps(my_obj))
        f.close
