66da6b664eb173662c902cd558d059435780627b
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-23 21:51:18 +0200

    taskk 2

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,7 +6,5 @@ def append_write(filename="", text=""):
     """function"""
     with open(filename, 'a') as f:
         f.write(text)
-    
-    with open(filename, "r", encoding="utf-8") as g:
-        contents = g.read()
-    return len(contents)
+
+    return len(text)
