26ea3e90dadc13cc695f85c709dcbafaa36e2ef6
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-28 01:25:44 +0200

    test

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -72,10 +72,11 @@ class Rectangle(Base):
                 print("{}".format("#"), end="")
             print("")
     
-    def __str__(self):
-        """comment"""
-        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                         self.__x,
-                                                        self.__y,
-                                                        self.__width,
-                                                        self.__height))
+    def __str__(self) -> str:
+        """
+        __str__ method
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.__x, self.__y,
+                                                       self.__width,
+                                                       self.__height)
