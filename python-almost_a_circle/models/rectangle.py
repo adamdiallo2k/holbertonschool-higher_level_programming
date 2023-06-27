1187942d69249dbc6c864b91e3d72134bb3a4c9b
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-28 01:00:26 +0200

    fix the space

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -60,6 +60,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
-    
+
     def area(self):
+        """commented function"""
         return self.__width * self.__height
