1fa700c41657f9329a615586993782834105b205
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-28 03:04:43 +0200

    task square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -25,3 +25,23 @@ class Square(Rectangle):
         """Return the print() and str() representation of a Square"""
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width)
+    
+    def update(self, *args, **kwargs):
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            if 'id' in kwargs:
+                self.id = kwargs['id']
+            if 'size' in kwargs:
+                self.size = kwargs['size']
+            if 'x' in kwargs:
+                self.x = kwargs['x']
+            if 'y' in kwargs:
+                self.y = kwargs['y']
