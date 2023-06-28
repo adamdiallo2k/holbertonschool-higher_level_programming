362ab7c5c53720ce2685992b08e10705d0f94027
AdamDiallo <adamdiallo2k@gmail.com>
2023-06-28 02:43:31 +0200

    task 11

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,14 +1,20 @@
-#!/usr/bin/python3
-"""commented module"""
-from models.rectangle import Rectangle
-
-
 class Square(Rectangle):
     """Class Square"""
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a Square instance"""
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get the size of the Square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the Square"""
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """Return the print() and str() representation of a Square"""
