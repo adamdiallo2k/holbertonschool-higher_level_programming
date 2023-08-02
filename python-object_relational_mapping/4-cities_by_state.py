173de2fe215960a0bb46b55f84259b3cf720589e
AdamDiallo <adamdiallo2k@gmail.com>
2023-08-03 00:45:45 +0200

    forgot inner join

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,9 @@ if __name__ == "__main__":
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT id, name, state_id FROM cities ORDER BY cities.id ASC")
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities
+                INNER JOIN states ON cities.state_id = states.id
+                ORDER BY cities.id ASC""")
     rows = cur.fetchall()
     for row in rows:
         print(row)
