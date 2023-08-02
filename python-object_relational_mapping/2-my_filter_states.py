29d5ed4863f47ce13e7d994482eb37548588a4ff
AdamDiallo <adamdiallo2k@gmail.com>
2023-08-02 16:58:42 +0200

    task 3

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""List all states from a given db sorted in ascending order by id"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    user_db = sys.argv[1]
+    passwd_db = sys.argv[2]
+    name_db = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+
+    db = MySQLdb.connect(
+        host="localhost", user=user_db, passwd=passwd_db, db=name_db, port=3306
+    )
+
+    querie = db.cursor()
+
+    querie.execute("SELECT * FROM states WHERE name == '%s' 'N%' ORDER BY states.id ASC", state_name_searched)
+
+    results = querie.fetchall()
+
+    for element in results:
+        print(element)
+
+    db.close()
