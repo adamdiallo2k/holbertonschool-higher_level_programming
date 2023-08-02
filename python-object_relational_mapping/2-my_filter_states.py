74d07f364fb416ca3b59c8775b3b895bc49a41bc
AdamDiallo <adamdiallo2k@gmail.com>
2023-08-03 00:36:57 +0200

    test

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,8 @@ if __name__ == "__main__":
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute(
+
+    cursor.execute(
         """
         SELECT *
         FROM states
