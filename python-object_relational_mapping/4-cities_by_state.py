291104be7dac60d9aaaac630fda0dc645e09d3aa
AdamDiallo <adamdiallo2k@gmail.com>
2023-08-03 00:42:05 +0200

    add the name and state id

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY cities.id ASC")
+    cur.execute("SELECT id, name, state_id FROM cities ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
