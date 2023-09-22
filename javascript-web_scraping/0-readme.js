d67735662abe700016710b427f580f4164a2eeff
AdamDiallo <adamdiallo2k@gmail.com>
2023-09-22 13:30:55 +0200

    test

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,16 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-
-        console.error(err);
-        return;
-    }
-
-    console.log(data.trim());
-}); 
+fs.readFile(process.argv[2], 'utf8', (error, data) => {
+  if (error) { console.log(error); } else { console.log(data); }
+});
