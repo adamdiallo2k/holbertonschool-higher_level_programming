f1dd1113fe4bd54b5bf8fecd6787b7f094bffde5
AdamDiallo <adamdiallo2k@gmail.com>
2023-09-22 13:30:15 +0200

    test

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,16 +2,15 @@
 
 const fs = require('fs');
 
-// Get the file path from the command line arguments
+
 const filePath = process.argv[2];
 
-// Read the file
 fs.readFile(filePath, 'utf-8', (err, data) => {
     if (err) {
-        // Print the error object if there's an error
+
         console.error(err);
         return;
     }
-    // Print the content of the file
+
     console.log(data.trim());
-});
+}); 
